1fa1b21beaa80770405007fc4134c43429f2159a
tzicatl <tzicatl@gmail.com>
2011-07-07 22:55:16 -0500

    Update fanstatic resource locations

--- src/logografo/resource.py
+++ src/logografo/resource.py
@@ -1,5 +1,10 @@
 from fanstatic import Library, Resource
 
+#Library
 library = Library('logografo', 'static')
 
+#Resources
 style = Resource(library, 'style.css')
+logografo_timeline = Resource(library, 'logografo_timeline.js')
+jquery_min = Resource(library, 'jquery-1.6.2.min.js')
+
